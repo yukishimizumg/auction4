e9fa51a87a38bdf5ae56dbb8ba5223693e920c86
yukishimizumg <marbleday.yukimay8@gmail.com>
2019-05-02 15:47:36 +0900

    new,edit,delete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,4 +11,32 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to item_path(@item.id)
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
+  end
+
 end
